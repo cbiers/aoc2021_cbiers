e6bb8ae14c5ad12857aeb6c4b854b0c21a28093e
cbiers <chris.biersbach@gmail.com>
2021-12-10 08:40:56 +0100

    Day 10 Part 2 done

---
+++ 10/2/2.py
@@ -0,0 +1,36 @@
+f = open("in.txt", "r")
+lines = f.readlines()
+
+closing = {")": 3, "]": 57, "}": 1197, ">": 25137}
+
+scores = []
+
+for line in lines:
+    stack = []
+    corrupted = False
+    for c in line[:-1]:
+        if c not in closing.keys():
+            stack.append(c)
+        else:
+            o = stack.pop()
+            if not(c == ")" and o == "(" or c == "]" and o == "[" or c == ">" and o == "<" or c == "}" and o == "{"):
+                corrupted = True
+                break
+    if not corrupted:
+        val = 0
+        while len(stack) > 0:
+            print stack
+            val *= 5
+            el = stack.pop()
+            if el == "(":
+                val += 1
+            elif el == "[":
+                val += 2
+            elif el == "{":
+                val += 3
+            else:
+                val += 4
+        scores.append(val)
+
+scores.sort()
+print scores[len(scores)/2]
