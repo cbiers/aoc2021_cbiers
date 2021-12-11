0853f70665b4cdb06a328949ae40718b2f060208
cbiers <chris.biersbach@gmail.com>
2021-12-11 06:32:13 +0100

    Day 11 Part 1 Done

---
+++ 11/1/1.py
@@ -0,0 +1,67 @@
+def flash(data, x, y, flashed):
+    flashed[x][y] = True
+    flashes = 1
+    data[x][y] = 0
+    if x != 0:
+        if y != 0:
+            if not flashed[x-1][y-1]:
+                data[x-1][y-1] += 1
+                if data[x-1][y-1] > 9:
+                    flashes += flash(data, x-1, y-1, flashed)
+        if not flashed[x-1][y]:
+            data[x-1][y] += 1
+            if data[x-1][y] > 9:
+                flashes += flash(data, x-1, y, flashed)
+        if y != len(data)-1:
+            if not flashed[x-1][y+1]:
+                data[x-1][y+1] += 1
+                if data[x-1][y+1] > 9:
+                    flashes += flash(data, x-1, y+1, flashed)
+    if x != len(data)-1:
+        if y != 0:
+            if not flashed[x+1][y-1]:
+                data[x+1][y-1] += 1
+                if data[x+1][y-1] > 9:
+                    flashes += flash(data, x+1, y-1, flashed)
+        if not flashed[x+1][y]:
+            data[x+1][y] += 1
+            if data[x+1][y] > 9:
+                flashes += flash(data, x+1, y, flashed)
+        if y != len(data)-1:
+            if not flashed[x+1][y+1]:
+                data[x+1][y+1] += 1
+                if data[x+1][y+1] > 9:
+                    flashes += flash(data, x+1, y+1, flashed)
+    if y != 0:
+        if not flashed[x][y-1]:
+            data[x][y-1] += 1
+            if data[x][y-1] > 9:
+                flashes += flash(data, x, y-1, flashed)
+    if y != len(data)-1:
+        if not flashed[x][y+1]:
+            data[x][y+1] += 1
+            if data[x][y+1] > 9:
+                flashes += flash(data, x, y+1, flashed)
+    return flashes
+
+f = open("in.txt", "r")
+lines = f.readlines()
+
+data = []
+
+for line in lines:
+    data.append([int(x) for x in line[:-1]])
+
+flashes = 0
+
+for i in range(100):
+    for j in range(len(data)):
+        for k in range(len(data)):
+            data[j][k] += 1
+    flashed = [[False for i in range(len(data))] for j in range(len(data))]
+    for j in range(len(data)):
+        for k in range(len(data)):
+            if data[j][k] > 9:
+                flashes += flash(data, j, k, flashed)
+
+print flashes
