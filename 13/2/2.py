f926f4bdc4baad641e6d83a150e74787cc3e7a9f
cbiers <chris.biersbach@gmail.com>
2021-12-13 08:19:32 +0100

    Day 13 Part 2 done

--- 13/2/2.py
+++ 13/2/2.py
@@ -11,6 +11,19 @@ def foldX(points, val):
     for i in toDelete:
         points.pop(i)
 
+def foldY(points, val):
+    toDelete = []
+    for i in range(len(points)):
+        point = points[i]
+        if point[1] > val:
+            newPoint = [point[0], val - (point[1] - val)]
+            if newPoint not in points:
+                points.append(newPoint)
+            toDelete.append(i)
+    toDelete.sort(reverse=True)
+    for i in toDelete:
+        points.pop(i)
+
 f = open("in.txt", "r")
 lines = f.readlines()
 
@@ -28,10 +41,26 @@ for line in lines:
         fold = line[11:].split("=")
         folds.append({"axis": fold[0], "val": int(fold[1])})
 
-for fold in folds[:]:
+for fold in folds:
     if fold["axis"] == "x":
         foldX(points, fold["val"])
     else:
         foldY(points, fold["val"])
 
-print len(points)
+maxX = 0
+maxY = 0
+
+for point in points:
+    if point[0] > maxX:
+        maxX = point[0]
+    if point[1] > maxY:
+        maxY = point[1]
+
+for i in range(maxY+1):
+    s = ""
+    for j in range(maxX+1):
+        if [j, i] in points:
+            s += "#"
+        else:
+            s += "."
+    print s
